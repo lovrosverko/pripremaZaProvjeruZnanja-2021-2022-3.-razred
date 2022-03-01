73a2a8ca54d55cb23a393dbf0162f47cac63f46e
Lovro Šverko <lovro.sverko@gmail.com>
2022-03-01 12:08:09 +0100

    Add files via upload

---
+++ Priprema za provjeru znanja 2122/Priprema za provjeru znanja 2122/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Priprema_za_provjeru_znanja_2122
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Priprema za provjeru znanja 2122/Priprema za provjeru znanja 2122/Vozilo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_za_provjeru_znanja_2122
+{
+    internal class Vozilo
+    {
+        String vrsta, marka, naziv;
+        DateTime GodPro;
+        int snaga;
+
+        public Vozilo(string vrsta, string marka, 
+            string naziv, DateTime godPro, int snaga)
+        {
+            this.vrsta = vrsta;
+            this.marka = marka;
+            this.naziv = naziv;
+            GodPro = godPro;
+            this.snaga = snaga;
+        }
+
+        public string Vrsta { get => vrsta; set => vrsta = value; }
+        public string Marka { get => marka; set => marka = value; }
+        public string Naziv { get => naziv; set => naziv = value; }
+        public DateTime GodPro1 { get => GodPro; set => GodPro = value; }
+        public int Snaga { get => snaga; set => snaga = value; }
+
+        public override string ToString()
+        {
+            String ispis = "Vrsta: " + this.Vrsta + "\nMarka: " + this.Marka
+                + "\nNaziv: " + this.Naziv + "\nGodina proizvodnje: "
+                + this.GodPro1.Year + "\nSnaga: " + this.snaga + " KS";
+            return ispis;
+        }
+    }
+}
